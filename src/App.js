52ae8ff0f4b1a55401b4194940ab3082989fb707
Teddy <wjddns363@naver.com>
2022-10-25 23:55:40 +0900

    5. ref:DOM에 이름달기

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
-import EventPractice from "./EventPractice";
+import React, { Component } from "react";
+import ScrollBox from "./ScrollBox";
 
-const App = () => {
-  return <EventPractice />;
-};
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <ScrollBox ref={(ref) => this.ScrollBox=ref}/>
+        <button onClick={() => this.ScrollBox.scrollToBottom()}>
+          맨 밑으로
+        </button>
+      </div>
+    );
+  }
+}
 
 export default App;
